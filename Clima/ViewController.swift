551f7c11b210b355476e2f1b9319e3813d92ac6e
Alumno <ulsa@iMacD103.local>
2018-10-23 18:56:04 -0700

    Clima

--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -10,15 +10,52 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-    @IBOutlet weak var lblGrados: UILabel!
+    let urlCiudad = "https://api.openweathermap.org/data/2.5/weather?q=ciudad+obregon&APPID=3326d4de85a204e001116973cc1e557a&units=metric"
     
+    @IBOutlet weak var lblGrados: UILabel!
+    @IBOutlet weak var aiCargarClima: UIActivityIndicatorView!
+    @IBAction func doTapActualizarClima(_ sender: Any) {
+        lblGrados.text = "-"
+        aiCargarClima.startAnimating()
+        Alamofire.request(urlCiudad).responseJSON { response in
+            //Diccionario Respuesta
+            if let  dictRespuesta = response.result.value as? NSDictionary {
+                
+                //Diccionario Main
+                if let dictMain = dictRespuesta.value(forKey: "main") as? NSDictionary {
+                    if let temp = dictMain.value(forKey: "temp") as? Double {
+                        self.lblGrados.text = "\(temp)"
+                        self.aiCargarClima.stopAnimating()
+                    }
+                    
+                }
+            } else {
+                //Avisarle al usuario que no se pudo leer la respuesta
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlCiudad = "http://api.openweathermap.org/data/2.5/weather?q=ciudad+obregon&APPID=3326d4de85a204e001116973cc1e557a&units=metric"
+        aiCargarClima.startAnimating()
         
+        
+
         Alamofire.request(urlCiudad).responseJSON { response in
-            
+            //Diccionario Respuesta
+            if let  dictRespuesta = response.result.value as? NSDictionary {
+                
+                //Diccionario Main
+                if let dictMain = dictRespuesta.value(forKey: "main") as? NSDictionary {
+                    if let temp = dictMain.value(forKey: "temp") as? Double {
+                        self.lblGrados.text = "\(temp)"
+                        self.aiCargarClima.stopAnimating()
+                    }
+                    
+                }
+            } else {
+                //Avisarle al usuario que no se pudo leer la respuesta
+            }
         }
     }
 
